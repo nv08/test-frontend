c1fc8e748caeb7df446e9754e070c26eb7bb6d20
Nilay Varshney <nyvy.07@gmail.com>
2022-09-11 11:41:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,15 +3,12 @@ const express = require('express')
 
 const app = express()
 
-const GOOGLE_CLIENT_ID = "834584514103-b22ev5ge0qj7qb9bts30gkco60qt2cop.apps.googleusercontent.com"
-const GOOGLE_CLIENT_SECRET = "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"
+
 
 app.get('/', async (req, res) => {
-    let image = await axios('https://source.unsplash.com/random');
-    res.send(`<h1> hey yo ghcr with perfect action file!</h1>`)
+    res.send('hello');
 })
 
-
 app.listen((3000), () => {
-    console.log("listening on port 3000 with pipeline!")
+    console.log("listening on port 3000 with pipeline!");
 })
